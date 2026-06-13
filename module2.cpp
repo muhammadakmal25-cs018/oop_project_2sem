e437f7b5591fdb1e291bdeed8cdaf149d725d216
muhammadakmal25-cs018 <muhammadakmal.7975@gmail.com>
2026-06-13 07:13:05 +0500

    2 module
    2nd

---
+++ module2.cpp
@@ -0,0 +1,128 @@
+// courses & enrollment management (encapsulation, operator overloading)
+#include<iostream>
+#include<string>
+#include <vector>   
+#include<stdexcept>
+using namespace std;
+// exception
+class capacityexceeded:public exception{
+    public:
+    const char* what() const noexcept override{
+        return "Course capacity exceeded. Cannot enroll more students.";
+    }
+};
+// student clas
+class student{
+    private:
+    string id;
+    string name;
+public:
+    student(string id,string name){
+        this->id=id;
+        this->name=name;
+    }
+    string getid(){
+        return id;
+    }
+    string getname(){
+        return name;
+    }
+};
+//  faculty class
+class faculty{
+    private:
+    string id;
+    string name;   
+public:    faculty(string id,string name){
+        this->id=id;
+        this->name=name;
+    }
+    string getid(){
+        return id;
+    }
+    string getname(){
+        return name;
+    }
+};
+//course class
+class course{
+    private:
+    string Cours_code;
+    string Cours_name;
+    int cradit_hors;
+    faculty* instructor;
+    int max_capacity;
+    int enrol_count;
+    vector<student*> enrolled_students;
+    vector<student*> waitlist;
+public:
+    course(string code,string name,int credit,faculty* instructor,int capacity):Cours_code(code),Cours_name(name),cradit_hors(credit),instructor(instructor),max_capacity(capacity),enrol_count(0){}
+   string getcors_code(){
+        return Cours_code;
+    }
+
+    void enroll(student* s){
+        if(enrol_count>=max_capacity)
+        throw capacityexceeded();
+        enrolled_students.push_back(s);
+        enrol_count++;
+    }
+//add waiting list
+void addtowaitlist(student* s){
+    waitlist.push_back(s);
+}
+//Operator ==
+
+bool operator==(const course& c)
+{
+return Cours_code ==c.Cours_code;
+}
+// Operator +
+vector<student*> operator+(course& c) {
+vector<student*> merged =waitlist;
+for (auto s:c.waitlist) {
+
+merged. push_back(s);
+}
+return merged;
+}
+// friend operator<<
+friend ostream& operator<<(ostream& os,const course& c){
+    os<<"Course Code: "<<c.Cours_code<<endl;
+    os<<"Course Name: "<<c.Cours_name<<endl;
+    os<<"Credit Hours: "<<c.cradit_hors<<endl;
+    os<<"Instructor: "<<c.instructor->getname()<<endl;
+    os<<"capacity: "<<c.max_capacity<<endl;
+    os<<"Enrolled Students: "<<c.enrolled_students.size()<<endl;
+    return os;
+}
+};
+// enrlment class
+class enrollment{
+    student* s;
+    course* c;
+    string enrollment_date;
+    char grade;
+public:
+    enrollment(student* s,course* c,string date):s(s),c(c),enrollment_date(date),grade('I'){}
+    void assigngrade(char g){
+        grade=g;
+    }
+};
+int main(){
+    faculty f1("F001","Dr. ali");
+    course c1("CS101","Intro to CS",3,&f1,2);
+    student s1("S001","Ahmed");
+    student s2("S002","Bilal");
+    student s3("S003","Awais");
+    try{
+        c1.enroll(&s1);
+        c1.enroll(&s2);
+        c1.enroll(&s3); // This will throw an exception
+    }catch(const capacityexceeded& e){
+        cout<<e.what()<<endl;
+        c1.addtowaitlist(&s3);
+    }
+    cout<<c1;
+    return 0;
+}
